39e410bdb12458edd06d590d7bf9e356aacba94f
ArjunPatidarRadix <radixdt.2530@gmail.com>
2024-08-14 12:08:51 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const fs = require("fs");
-const https = require("https");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -30,8 +29,8 @@ const store = MongoDBStore({
 });
 
 const csrfProtection = csrf();
-const privateKey = fs.readFileSync("server.key");
-const certificate = fs.readFileSync("server.cert");
+// const privateKey = fs.readFileSync("server.key");
+// const certificate = fs.readFileSync("server.cert");
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
